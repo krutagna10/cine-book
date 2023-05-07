b319f8f6708c975383c55b2cc2246704c96ced97
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-05-07 08:47:58 +0530

    readme updated

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -16,17 +16,18 @@ function CartItem({ item }) {
   return (
     <li className="cart-item">
       <div>
-        <h2>{item.name}</h2>
+        <h2>
+          {item.name} (x {item.quantity})
+        </h2>
         <div className="cart-item__summary">
           <span className="cart-item__price">${item.price.toFixed(2)}</span>
-          <span className="cart-item--quantity">x {item.quantity}</span>
         </div>
       </div>
       <div className="cart-item__actions">
-        <button className="cart-item__button" onClick={handleRemoveItem}>
+        <button className="cart-item__increment-btn" onClick={handleRemoveItem}>
           −
         </button>
-        <button className="cart-item__button" onClick={handleAddItem}>
+        <button className="cart-item__decrement-btn" onClick={handleAddItem}>
           +
         </button>
       </div>
